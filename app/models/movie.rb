ddad7d70ad42fabe48f6473d3d8999a1a28912f6
pmezamoc <pmezamoc@chicagobooth.edu>
2023-02-23 03:00:34 +0000

    progress

---
+++ app/models/movie.rb
@@ -0,0 +1,32 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  duration    :integer
+#  image       :string
+#  title       :string
+#  year        :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  director_id :integer
+#
+class Movie < ApplicationRecord
+
+  has_many(:characters, { :class_name => "Character", :foreign_key => "movie_id", :dependent => :destroy })
+
+
+  has_many(:bookmarks, { :class_name => "Bookmark", :foreign_key => "movie_id", :dependent => :destroy })
+
+
+  belongs_to(:director, { :required => true, :class_name => "Director", :foreign_key => "director_id" })
+
+
+  has_many(:actors, { :through => :characters, :source => :actor })
+
+
+  has_many(:users, { :through => :bookmarks, :source => :user })
+
+
+end
